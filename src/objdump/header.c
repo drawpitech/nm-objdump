4e0bfb4ace55da059cf668ace0821f8e08147136
Clément <clement2104.boillot@gmail.com>
2024-03-06 18:01:40 +0100

    Move first print in the main function

--- src/objdump/header.c
+++ src/objdump/header.c
@@ -13,15 +13,12 @@
 
 int print_header(binary_t *bin)
 {
-
     if (bin == NULL)
         return RET_ERROR;
     printf(
-        "\n%s:     file format %s\n"
         "architecture: i386:%s, flags 0x%08lx:\n"
         "\n"
         "start address 0x%016lx\n\n",
-        bin->filename, "elf64-x86-64", "x86-64", bin->shdr->sh_flags,
-        bin->ehdr->e_entry);
+        "x86-64", bin->shdr->sh_flags, bin->ehdr->e_entry);
     return RET_VALID;
 }
--- src/objdump/objdump.c
+++ src/objdump/objdump.c
@@ -19,6 +19,7 @@ int my_objdump(UNUSED int argc, char **argv)
     if (!get_args(argv, &bin, LEN_OF(OB_ARGS), OB_ARGS) ||
         binary_open(&bin) == NULL)
         return RET_ERROR;
+    printf("\n%s:     file format elf%d-%s\n", bin.filename, 64, "x86-64");
     if (bin.args & FLG_HEADER && print_header(&bin) == RET_ERROR) {
         binary_free(&bin);
         return RET_ERROR;
