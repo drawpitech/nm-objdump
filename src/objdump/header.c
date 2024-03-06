c9d19b929d7d2032d81c46b1dc8542830987a269
Clément <clement2104.boillot@gmail.com>
2024-03-06 17:45:10 +0100

    Fix start address in objdump

--- src/objdump/header.c
+++ src/objdump/header.c
@@ -13,22 +13,15 @@
 
 int print_header(binary_t *bin)
 {
-    Elf64_Shdr *start_addr = NULL;
 
     if (bin == NULL)
         return RET_ERROR;
-    start_addr = binary_get_table(bin, 0, ".init");
-    if (start_addr == NULL) {
-        binary_free(bin);
-        fprintf(stderr, "%s: No section named .init\n", PROGNAME);
-        return RET_ERROR;
-    }
     printf(
         "\n%s:     file format %s\n"
         "architecture: i386:%s, flags 0x%08lx:\n"
         "\n"
         "start address 0x%016lx\n\n",
         bin->filename, "elf64-x86-64", "x86-64", bin->shdr->sh_flags,
-        start_addr->sh_addr);
+        bin->ehdr->e_entry);
     return RET_VALID;
 }
