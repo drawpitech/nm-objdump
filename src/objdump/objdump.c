02c484a3543cab2d77850ffd2d9add84c6b8162e
Clément <clement2104.boillot@gmail.com>
2024-03-07 18:06:50 +0100

    Add type to objdump output

--- src/objdump/objdump.c
+++ src/objdump/objdump.c
@@ -19,7 +19,9 @@ int my_objdump(UNUSED int argc, char **argv)
     if (!get_args(argv, &bin, LEN_OF(OB_ARGS), OB_ARGS) ||
         binary_open(&bin) == NULL)
         return RET_ERROR;
-    printf("\n%s:     file format elf%d-%s\n", bin.filename, 64, "x86-64");
+    printf(
+        "\n%s:     file format elf%d-%s\n", bin.filename,
+        bin.ehdr->e_ident[EI_CLASS] ? 64 : 32, "x86-64");
     if (bin.args & FLG_HEADER && print_header(&bin) == RET_ERROR)
         return RET_ERROR;
     printf("\n");
