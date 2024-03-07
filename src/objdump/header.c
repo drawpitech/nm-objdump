c7a8cff6a5c8cc7a13cfde25f8f95830d6391ce0
Clément <clement2104.boillot@gmail.com>
2024-03-07 19:25:03 +0100

    Fix architecture in objdump -f

--- src/objdump/header.c
+++ src/objdump/header.c
@@ -11,14 +11,24 @@
 #include "objdump.h"
 #include "utils.h"
 
+static const char *const EVENT_TABLE[] = {
+    [EM_X86_64] = "x86-64",
+    [EM_386] = "i386",
+};
+
 int print_header(binary_t *bin)
 {
+    const char *arch = NULL;
+
     if (bin == NULL)
         return RET_ERROR;
+    arch = EVENT_TABLE[bin->ehdr->e_machine];
+    if (arch == NULL)
+        arch = "Unknown";
     printf(
         "architecture: i386:%s, flags 0x%08lx:\n"
         "\n"
         "start address 0x%016lx\n",
-        "x86-64", bin->shdr->sh_flags, bin->ehdr->e_entry);
+        arch, bin->shdr->sh_flags, bin->ehdr->e_entry);
     return RET_VALID;
 }
